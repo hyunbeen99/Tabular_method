a147ad7d3cce040f94dd8fb527e7e4f47b563c33
Hyeonbeen <gusqls1124@gmail.com>
2020-05-30 01:22:57 +0900

    Find prime implicants

--- tabular_method.py
+++ tabular_method.py
@@ -1,1 +1,19 @@
-print("hello world")
+# change decimal to 4bit binary
+def decTobin(num):
+    x = num
+    y = ""
+    while x > 0:
+        y = str(x % 2) + y
+        x /= 2
+    return y
+
+# to get HD
+def hammingDistance(a, b):
+    first = []
+    second = []
+    first.append(decTobin(a))
+    second.append(decTobin(b))
+
+    for i in range 4:
+        if (first[i] != second[i]):
+            count += 1
